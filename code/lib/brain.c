ac40b44c9c98073cbdb7e89bc24d0edb9794011c
molinluca <molinluca01@gmail.com>
2020-11-30 17:25:00 +0100

    Game state check function

--- code/lib/brain.c
+++ code/lib/brain.c
@@ -1,17 +1,41 @@
 #include "../headers/brain.h"
 static short game_turn = USR_TEAM;
 
-void play(short i, short dir){
+short play(short i, short dir){
     Piece* p=getPiece(i);
 
-    if(p==NULL) return;
-    if(dir<0 || dir>3) return;
+    if(p==NULL) return 0;
+    if(dir<0 || dir>3) return 0;
     if(game_turn==getTeam(p)){
         calculate(p);
+
         if(p->moves[dir].score>0) {
             move(p, dir);
             game_turn = (game_turn == CPU_TEAM) ? USR_TEAM : CPU_TEAM;
+            return 1;
         }
     }
+    return 0;
+}
+
+short canPlayerMove(int team){
+    if(team==game_turn){
+        int i, j, pieces=0, moves=0;
+        for(i=0;i<22;i++){
+            Piece* p=getPiece(i);
+
+            if(p!=NULL){
+                if(getTeam(p)==team){
+                    pieces++;
+                    for(j=0;j<4;j++){
+                        if(p->moves[j].score>0)
+                            moves++;
+                    }
+                }
+            }
+        }
+        return (short)((moves>0) && (pieces>0));
+    }
+    return 1;
 }
 
